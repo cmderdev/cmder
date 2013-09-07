2c0a6d095f612d8e2a4c4e0443d14a6e7815115d
Samuel Vasko <samvasko@gmail.com>
2013-09-07 17:39:24 +0200

    Changes for startup

--- vendor/init.bat
+++ vendor/init.bat
@@ -1,25 +1,23 @@
-@echo off
-
 :: Set prompt style
-prompt $E[1;32;40m$P $_$E[1;30;40m$$ $E[0m
+@prompt $E[1;32;40m$P $_$E[1;30;40m$$ $E[0m
 
 :: Pick right version of clink
-if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+@if "%PROCESSOR_ARCHITECTURE%"=="x86" (
     set architecture=86
 ) else (
     set architecture=64
 )
 
 :: Run clink
-vendor\clink\clink_x%architecture%.exe inject --quiet --profile config
+@vendor\clink\clink_x%architecture%.exe inject --quiet --profile config
 
-set rootDir=%CD%
+@set rootDir=%CD%
 
 :: Enhance Path
-PATH=%PATH%;%rootDir%\bin
+@set PATH=%PATH%;%rootDir%\bin;
 
 :: Add aliases
-doskey /macrofile=%rootDir%\config\aliases
+@doskey /macrofile=%rootDir%\config\aliases
 
 :: cd into users homedir
-cd /d "%userprofile%"
+@cd /d "%userprofile%"
