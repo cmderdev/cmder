340d70a5966cff9322d81b3bce565e63fcc1c2fc
Samuel Vasko <samvasko@gmail.com>
2013-12-07 21:44:33 +0100

    This somehow works in git folder

--- config/prompt.lua
+++ config/prompt.lua
@@ -2,4 +2,4 @@ function lambda_prompt_filter()
     clink.prompt.value = string.gsub(clink.prompt.value, "{lamb}", "λ")
 end
 
-clink.prompt.register_filter(lambda_prompt_filter, 60)
+clink.prompt.register_filter(lambda_prompt_filter, 40)
