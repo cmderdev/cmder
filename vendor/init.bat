b4b9e4e7dcb9e6cc66359c406111a8ba58e6cef0
exoticknight <draco.knight0@gmail.com>
2013-12-10 19:30:50 +0800

    Update init.bat
    no error now if there are spaces in path

--- vendor/init.bat
+++ vendor/init.bat
@@ -2,38 +2,43 @@
 :: Sets some nice defaults
 :: Created as part of cmder project
 
-:: Change the prompt style
-:: Mmm tasty lamb
-@prompt $E[1;32;40m$P$S{git}$S$_$E[1;30;40m{lamb}$S$E[0m
+@echo off
+:: Setting prompt style
+for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
+
+:: The slow part
+:: World without Unicode is a sad world
+chcp 65001>nul
+:: It has to be lambda, I already made a logo
+prompt $E[1;32;40m$P$S{git}$S$_$E[1;30;40mλ$S$E[0m
+chcp %cp%>nul
 
 :: Pick right version of clink
-@if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+if "%PROCESSOR_ARCHITECTURE%"=="x86" (
     set architecture=86
 ) else (
     set architecture=64
 )
 
-@set rootDir="%~dp0\.."
+::get the parent directory
+for %%i in ("%~dp0.") do set rootDir=%%~dpi
 
 :: Run clink
-@%rootDir%\vendor\clink\clink_x%architecture%.exe inject --quiet --profile %rootDir%\config
+"%rootDir%vendor\clink\clink_x%architecture%.exe" inject --quiet --profile "%rootDir%config"
 
 :: Prepare for msysgit
 
 :: I do not even know, copypasted from their .bat
-@set PLINK_PROTOCOL=ssh
-@if not defined TERM set TERM=cygwin
+set PLINK_PROTOCOL=ssh
+if not defined TERM set TERM=msys
 
 :: Enhance Path
-@set git_install_root=%rootDir%\vendor\msysgit
-@set PATH=%PATH%;%rootDir%\bin;%git_install_root%\bin;%git_install_root%\mingw\bin;%git_install_root%\cmd;%git_install_root%\share\vim\vim73;
+set git_install_root=%rootDir%vendor\msysgit
+set PATH=%PATH%;%rootDir%bin;%git_install_root%bin;%git_install_root%mingw\bin;%git_install_root%cmd;%git_install_root%share\vim\vim73;
 
 :: Add aliases
-@doskey /macrofile="%rootDir%\config\aliases"
+doskey /macrofile="%rootDir%config\aliases"
 
 :: Set home path
-@set HOME=%USERPROFILE%
-
-@if defined CMDER_START cd /d "%CMDER_START%"
-
-@echo Welcome to cmder!
+set HOME=%USERPROFILE%
+echo Welcome to cmder!
