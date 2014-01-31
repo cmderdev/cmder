bf882f1dc1f838f1aad85748b4458c05c8e7ca50
Samuel Vasko <samvasko@gmail.com>
2014-01-31 17:22:08 +0100

    Reordered the PATH, closing #37
    Now the msysgit is being loaded first, so find will invoke
    the correct and better version

--- vendor/init.bat
+++ vendor/init.bat
@@ -24,7 +24,7 @@
 
 :: Enhance Path
 @set git_install_root=%CMDER_ROOT%\vendor\msysgit
-@set PATH=%PATH%;%CMDER_ROOT%\bin;%git_install_root%\bin;%git_install_root%\mingw\bin;%git_install_root%\cmd;%git_install_root%\share\vim\vim73;
+@set PATH=%CMDER_ROOT%\bin;%git_install_root%\bin;%git_install_root%\mingw\bin;%git_install_root%\cmd;%git_install_root%\share\vim\vim73;%PATH%
 
 :: Add aliases
 @doskey /macrofile="%CMDER_ROOT%\config\aliases"
