370349bd88bb9ca8370793f6bed6e9da4e4d4651
Philip Crockett <phil.crockett@gmail.com>
2014-02-03 21:48:12 -0600

    Fixed PowerShell initial working directory problem when PowerShell task is the startup task.

--- scripts/powershell/CmderProfile.ps1
+++ scripts/powershell/CmderProfile.ps1
@@ -1,3 +1,7 @@
+if ($env:CMDER_START) {
+   Set-Location $env:CMDER_START
+}
+
 # Autoinclude every .ps1 script in the autorun folder.
 Get-ChildItem -Filter *.ps1 -File "$PSScriptRoot\autorun\" | ForEach-Object {
     . $_.FullName
