fccb80160f412e88ec3440412cdf0f1a3cccee86
sweiss3 <sweiss3@gmail.com>
2014-04-04 00:09:25 +0300

    Fixing auto-completion to take search for matches from the start + adding support for git branch -d

--- config/git_checkout.lua
+++ config/git_checkout.lua
@@ -1,7 +1,7 @@
 function git_checkout_match_generator(text, first, last)
 	found_matches = false;
-	if rl_state.line_buffer:find("^git checkout ") then
-		has_start_branch = not rl_state.line_buffer:find("^git checkout[ ]*$")
+	if rl_state.line_buffer:find("^git checkout ") or rl_state.line_buffer:find("^git branch %-d ") then
+		has_start_branch = (not rl_state.line_buffer:find("^git checkout[ ]*$")) and (not rl_state.line_buffer:find("^git branch %-d[ ]*$"))
 		for line in io.popen("git branch 2>nul"):lines() do
 			local m = line:match("[%* ] (.+)$")
 			
@@ -9,7 +9,7 @@ function git_checkout_match_generator(text, first, last)
 				if not has_start_branch then
 					clink.add_match(m)
 					found_matches = true;
-				elseif #text > 0 and m:find(text) then
+				elseif #text > 0 and m:find("^"..text) then
 					clink.add_match(m)
 					found_matches = true;
 				end
