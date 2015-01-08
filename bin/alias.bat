dba00b9175c5e90d7a28d562d8dc117f7516c46f
Vladimir Kotikov <vladimir.kotikov@akvelon.com>
2015-01-08 20:43:45 +0300

    Adds info about command options

--- bin/alias.bat
+++ bin/alias.bat
@@ -44,7 +44,12 @@ goto :eof
 
 :p_help
 echo.Usage:
-echo.	alias name=full command
+echo.	alias [/reload] [/d] [name=full command]
+echo.     /reload  Reload the aliases file
+echo.     /d       Delete an alias (must be followed by the alias name)
+echo.
+echo.	If alias is called with any parameters, it will display the list of existing aliases.
+echo.	In the command, you can use the following notations:
 echo.	$* allows the alias to assume all the parameters of the supplied command.
 echo.	$1-$9 Allows you to seperate parameter by number, much like %%1 in batch.
 echo.	$T is the command seperator, allowing you to string several commands together into one alias.
