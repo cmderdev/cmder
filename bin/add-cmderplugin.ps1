90fdeb9893bcd28b8dbcbe72dd7ec6f999d9a3d5
Dax T. Games <dax.games@ge.com>
2016-07-25 15:28:37 -0500

    Added profile modify functionality

--- bin/add-cmderplugin.ps1
+++ bin/add-cmderplugin.ps1
@@ -45,41 +45,32 @@ Param(
 )
 
 
-$ThisScript = Get-Item $MyInvocation.MyCommand.Definition
+# Get the scripts and cmder root dirs we are building in.
+$ScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$cmder_root = $ScriptRoot.replace("\bin","")
 
 # Dot source util functions into this scope
-. $($ThisScript.DirectoryName + "\utils.ps1")
+. $($ScriptRoot + "\utils.ps1")
 
-$bash_user_profile = $($env:cmder_root + "\config\user-profile.sh")
-$bash_user_aliases = $($env:cmder_root + "\config\user-aliases.sh")
+$bash_shared_profile = $($cmder_root + "\config\profile.d\001-shared-profile.sh")
+$bash_shared_aliases = $($cmder_root + "\config\profile.d\shared-aliases.sh")
 
-if (test-path $bash_user_aliases) {
-  $bash_aliases = $bash_user_aliases
-} else {
-  $bash_aliases = $bash_user_profile
-}
+$posh_shared_profile = $($cmder_root + "\config\profile.d\001-shared-profile.ps1")
+$posh_shared_aliases = $($cmder_root + "\config\profile.d\shared-aliases.ps1")
 
-$posh_user_profile = $($env:cmder_root + "\config\user-profile.ps1")
-$posh_user_aliases = $($env:cmder_root + "\config\user-aliases.ps1")
-
-if (test-path $posh_user_aliases) {
-  $posh_aliases = $posh_user_aliases
-} else {
-  $posh_aliases = $posh_user_profile
-}
-
-
-$cmder_user_profile = $($env:cmder_root + "\config\user-profile.cmd")
-$cmder_aliases = $($env:cmder_root + "\config\aliases")
+$cmder_shared_profile = $($cmder_root + "\config\profile.d\001-shared-profile.cmd")
+$cmder_shared_aliases = $($cmder_root + "\config\profile.d\shared-aliases.cmd")
 
 $ErrorActionPreference = "Stop"
-
-Push-Location -Path $saveTo
-$sources = Get-Content $PluginPath | Out-String | Convertfrom-Json
 
 # Check for requirements
 Ensure-Exists $PluginPath
 Ensure-Executable "7z"
+
+$sources = Get-Content $PluginPath | Out-String | Convertfrom-Json
+
+Push-Location -Path $saveTo
+
 New-Item -Type Directory -Path (Join-Path $saveTo "/tmp/") -ErrorAction SilentlyContinue >$null
 
 $vend = $pwd
@@ -96,7 +87,7 @@ foreach ($s in $sources) {
     Delete-Existing $tempArchive
 
     if ($force -eq $true) {
-      write-host $("Deleting existing install of the '" + $s.name + "' plugin...")
+      write-host $("`nDeleting existing install of the '" + $s.name + "' plugin...")
       Delete-Existing $s.name
     }
 
@@ -111,40 +102,92 @@ foreach ($s in $sources) {
           Flatten-Directory($s.name)
       }
 
-      foreach ($alias in $s.alias.psobject.properties) {
-        foreach ($shell in $alias.value.psobject.properties) {
+      foreach ($shared_alias in $s.shared_alias.psobject.properties) {
+        foreach ($shell in $shared_alias.value.psobject.properties) {
           if ($shell.name -eq "sh") {
-            $aliases = get-content $bash_aliases
-            $new_alias = $( "alias " + $alias.name + "='" + $shell.value + "'")
+            if (-not (test-path $bash_shared_aliases)) {
+              "# this is a shared aliases for the bash shell" | out-file $bash_shared_aliases -Encoding ASCII
+            }
+  
+            $aliases = get-content $bash_shared_aliases
+            $new_alias = $( "alias " + $shared_alias.name + "='" + $shell.value + "'")
             if ($aliases -notcontains $new_alias) {
-              write-host $("`nWriting alias '" + $alias.name + "' to '" + $bash_aliases + "'")
+              write-host $("`nWriting alias '" + $shared_alias.name + "' to '" + $bash_shared_aliases + "'")
               write-host $new_alias
-              $new_alias | Out-File $bash_aliases -Encoding ASCII -append
+              $new_alias | Out-File $bash_shared_aliases -Encoding ASCII -append
             }
           } elseif ($shell.name -eq "cmder") {
-            $aliases = get-content $cmder_aliases
-            $new_alias = $( $alias.name + "=" + $shell.value)
+            if (-not (test-path $cmder_shared_aliases)) {
+              ";= echo off" | out-file -Encoding ASCII $cmder_shared_aliases
+              ";= rem Call DOSKEY and use this file as the macrofile" | out-file -Encoding ASCII -append $cmder_shared_aliases
+              ";= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%" | out-file -Encoding ASCII -append $cmder_shared_aliases
+              ";= rem In batch mode, jump to the end of the file" | out-file -append -Encoding ASCII $cmder_shared_aliases
+              ";= set aliases=%cmder_root%\config\profile.d\shared-aliases.cmd" | out-file -Encoding ASCII -append $cmder_shared_aliases
+              ";= goto :eof" | out-file -Encoding ASCII -append $cmder_shared_aliases
+            }
+  
+            $aliases = get-content $cmder_shared_aliases
+            $new_alias = $( $shared_alias.name + "=" + $shell.value)
             if ($aliases -notcontains $new_alias) {
-              write-host $("`nWriting alias '" + $alias.name + "' to '" + $cmder_aliases + "'")
+              write-host $("`nWriting alias '" + $shared_alias.name + "' to '" + $cmder_shared_aliases + "'")
               write-host $new_alias
-              $new_alias | Out-File $cmder_aliases -Encoding ASCII -append
+              $new_alias | Out-File $cmder_shared_aliases -Encoding ASCII -append
             }
           } elseif ($shell.name -eq "posh") {
-            $aliases = get-content $posh_aliases
-            $new_alias = $( "new-alias " + $alias.name + " " + $($shell.value -replace "/","\") + " -erroraction silentlycontinue")
+            if (-not (test-path $posh_shared_aliases)) {
+              "# this is a shared aliases for Powershell" | out-file $posh_shared_aliases
+            }
+            
+            $aliases = get-content $posh_shared_aliases
+            $new_alias = $( "new-alias " + $shared_alias.name + " '" + $($shell.value -replace "/","\") + "' -erroraction silentlycontinue")
             if ($aliases -notcontains $new_alias) {
-              write-host $("`nWriting alias '" + $alias.name + "' to '" + $posh_aliases + "'")
+              write-host $("`nWriting alias '" + $shared_alias.name + "' to '" + $posh_shared_aliases + "'")
               write-host $new_alias
-              $new_alias | Out-File $posh_aliases -append
-              . $posh_aliases
-              get-alias $alias.name
+              $new_alias | Out-File $posh_shared_aliases -append
             }
           }
         }
       }
-
-      . $posh_aliases
-
+  
+      foreach ($shared_profile in $s.shared_profile.psobject.properties) {
+        foreach ($shell in $shared_profile.value.psobject.properties) {
+          if ($shell.name -eq "sh") {
+            if (-not (test-path $bash_shared_profile)) {
+              "# this is a shared profile for the bash shell" | out-file $bash_shared_profile -Encoding ASCII
+            }
+  
+            $profile_lines = get-content $bash_shared_profile
+            if ($profile_lines -notcontains $shell.value) {
+              write-host $("`nWriting to '" + $bash_shared_profile + "'")
+              write-host $shell.value
+              $shell.value | Out-File $bash_shared_profile -Encoding ASCII -append
+            }
+          } elseif ($shell.name -eq "cmder") {
+            if (-not (test-path $cmder_shared_profile)) {
+              "REM this is a shared profile for the cmd shell" | out-file $cmder_shared_profile -Encoding ASCII
+            }
+  
+            $profile_lines = get-content $cmder_shared_profile
+            if ($profile_lines -notcontains $shell.value) {
+              write-host $("`nWriting to '" + $cmder_shared_profile + "'")
+              write-host $shell.value
+              $shell.value | Out-File $cmder_shared_profile -Encoding ASCII -append
+            }
+          } elseif ($shell.name -eq "posh") {
+            if (-not (test-path $posh_shared_profile)) {
+              "# this is a shared profile for Powershell" | out-file $posh_shared_profile
+            }
+            
+            $profile_lines = get-content $posh_shared_profile
+            if ($profile_lines -notcontains $shell.value) {
+              write-host $("`nWriting to '" + $posh_shared_profile + "'")
+              write-host $shell.value
+              $shell.value | Out-File $posh_shared_profile -append
+            }
+          }
+        }
+      }
+  
       foreach ($post_command in $s.post_commands) {
         write-host $("`nRunning '" + $post_command + "'...")
         invoke-expression $post_command
@@ -156,6 +199,6 @@ foreach ($s in $sources) {
 
 Pop-Location
 
-Write-host "Restart ALL cmder sessions for the plugin install to be compplete!"
+Write-host "Restart ALL cmder sessions for the plugin install to be complete!"
 
 
