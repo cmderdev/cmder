7f3a7261b953f68155a483920cbe622dfa7213d8
Hugo-KTM <Hugo.Hinterberger@ktm.com>
2017-08-21 15:38:22 +0200

    make "Cmder Here" work in more cases
    integrated suggestion from comments (test for trailing backslash to prevent doubling of trailing backslash)
    integrated suggestions from #1456: switching ```cd /d``` over to ```pushd``` to get support for UNC-paths

--- vendor/init.bat
+++ vendor/init.bat
@@ -160,9 +160,11 @@ if not defined HOME set "HOME=%USERPROFILE%"
 :: This is either a env variable set by the user or the result of
 :: cmder.exe setting this variable due to a commandline argument or a "cmder here"
 if defined CMDER_START (
-    cd /d "%CMDER_START%\"
+    if "%CMDER_START:~-1%" neq "\" (
+        set CMDER_START="%CMDER_START%\"
+    )
+    pushd "%CMDER_START%"
 )
-
 
 if exist "%CMDER_ROOT%\config\user-profile.cmd" (
     REM Create this file and place your own command in there
