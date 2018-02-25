baf719e3e9700ccef95d1facb73c9d05f4484600
Dax T. Games <dtgames@kinggeek.org>
2018-02-25 09:57:59 -0600

    init.bat with args is executed outside cmder/conemu sets cmder_root properly. Thanks @DRSDavidSoft

--- vendor/init.bat
+++ vendor/init.bat
@@ -10,6 +10,22 @@
 set verbose-output=0
 set debug-output=0
 
+:: Find root dir
+if not defined CMDER_ROOT (
+    if defined ConEmuDir (
+        for /f "delims=" %%i in ("%ConEmuDir%\..\..") do (
+            set "CMDER_ROOT=%%~fi"
+        )
+    ) else (
+        for /f "delims=" %%i in ("%~dp0\..") do (
+            set "CMDER_ROOT=%%~fi"
+        )
+    )
+)
+
+:: Remove trailing '\' from %CMDER_ROOT%
+if "%CMDER_ROOT:~-1%" == "\" SET "CMDER_ROOT=%CMDER_ROOT:~0,-1%"
+
 :var_loop
     if "%~1" == "" (
         goto :start
@@ -47,31 +63,12 @@ goto var_loop
 
 :start
 
+call :debug-output init.bat - Env Var - CMDER_ROOT=%CMDER_ROOT%
 call :debug-output init.bat - Env Var - debug-output=%debug-output%
 
 if defined CMDER_USER_CONFIG (
     call :debug-output init.bat - CMDER IS ALSO USING INDIVIDUAL USER CONFIG FROM '%CMDER_USER_CONFIG%'!
 )
-
-:: Find root dir
-if not defined CMDER_ROOT (
-    call :debug-output init.bat - CMDER_ROOT is not defined!
-    if defined ConEmuDir (
-        for /f "delims=" %%i in ("%ConEmuDir%\..\..") do (
-            call :debug-output init.bat - Setting CMDER_ROOT based off of "%ConEmuDir%\..\.."
-            set "CMDER_ROOT=%%~fi"
-        )
-    ) else (
-        for /f "delims=" %%i in ("%~dp0\..") do (
-            call :debug-output init.bat - Setting CMDER_ROOT based off of "%~dp0\.."
-            set "CMDER_ROOT=%%~fi"
-        )
-    )
-)
-
-:: Remove trailing '\' from %CMDER_ROOT%
-if "%CMDER_ROOT:~-1%" == "\" SET "CMDER_ROOT=%CMDER_ROOT:~0,-1%"
-call :debug-output init.bat - Env Var - CMDER_ROOT=%CMDER_ROOT%
 
 :: Pick right version of clink
 if "%PROCESSOR_ARCHITECTURE%"=="x86" (
