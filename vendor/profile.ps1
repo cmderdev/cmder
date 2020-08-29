f05933deeb6cefff45f94648edb19ddeaa1b043e
Dax T. Games <dtgaes@kinggeek.org>
2020-08-28 20:23:21 -0400

    fix no git found in path

--- vendor/profile.ps1
+++ vendor/profile.ps1
@@ -43,7 +43,7 @@ $gitVersionVendor = (readVersion -gitPath "$ENV:CMDER_ROOT\vendor\git-for-window
 # write-host "GIT VENDOR: ${gitVersionVendor}"
 
 # Get user installed Git Version[s] and Compare with vendored if found.
-foreach ($git in (get-command 'git')) {
+foreach ($git in (get-command -ErrorAction SilentlyContinue 'git')) {
     $gitDir = Split-Path -Path $git.Path
     $gitDir = isGitShim -gitPath $gitDir 
     $gitVersionUser = (readVersion -gitPath $gitDir)
