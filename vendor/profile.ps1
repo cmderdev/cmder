8d4afb057051353fbd2f72425d1fd4714e7579c8
David Refoua <David@Refoua.me>
2022-10-16 00:02:38 +0330

    fix problem with #2756

--- vendor/profile.ps1
+++ vendor/profile.ps1
@@ -18,9 +18,9 @@ if ($ENV:CMDER_USER_CONFIG) {
 # We do this for Powershell as Admin Sessions because CMDER_ROOT is not being set.
 if ($null -eq $ENV:CMDER_ROOT) {
     if ($ENV:ConEmuDir) {
-        $ENV:CMDER_ROOT = Resolve-Path $ENV:ConEmuDir + "\..\.."
+        $ENV:CMDER_ROOT = Resolve-Path($ENV:ConEmuDir + "\..\..")
     } else {
-        $ENV:CMDER_ROOT = Resolve-Path $PSScriptRoot + "\.."
+        $ENV:CMDER_ROOT = Resolve-Path($PSScriptRoot + "\..")
     }
 }
 
