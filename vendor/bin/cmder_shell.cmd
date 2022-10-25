a2a996147e504eca923593ac4651c758a0c9a78c
David Refoua <David@Refoua.me>
2022-10-25 18:42:30 +0330

    adjust root path

--- vendor/bin/cmder_shell.cmd
+++ vendor/bin/cmder_shell.cmd
@@ -1,13 +1,13 @@
 @echo off
 
-set CMDER_ROOT=%~dp0..\
+set CMDER_ROOT=%~dp0..\..\
 
 if "%cmder_init%" == "1" (
-  "%CMDER_ROOT%\vendor\clink\clink.bat" inject -q --profile "%CMDER_ROOT%\config" --scripts "%CMDER_ROOT%\vendor"
+    "%CMDER_ROOT%\vendor\clink\clink.bat" inject -q --profile "%CMDER_ROOT%\config" --scripts "%CMDER_ROOT%\vendor"
 ) else (
-  set cmder_init=1
+    set cmder_init=1
 )
 
-pushd %CMDER_ROOT%
+pushd "%CMDER_ROOT%"
 call "%CMDER_ROOT%\vendor\init.bat" /f
 popd
