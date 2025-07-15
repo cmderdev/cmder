abcb1a1aa10edf3bcf5cd792c93f809aea456dfd
David Refoua <David@Refoua.me>
2025-07-15 19:26:49 +0330

    display MSYSTEM if set; fix indentation; add comment

--- vendor/git-prompt.sh
+++ vendor/git-prompt.sh
@@ -20,7 +20,7 @@ function getSimpleGitBranch() {
   then
     echo " (${headContent:16})"
   else
-  echo " (HEAD detached at ${headContent:0:7})"
+    echo " (HEAD detached at ${headContent:0:7})"
   fi
 }
 
@@ -38,12 +38,12 @@ then
     . ~/.config/git/git-prompt.sh
   fi
 else
-  PS1='\[\033]0;$MSYSTEM:${PWD//[^[:ascii:]]/?}\007\]' # set window title
-  # PS1="$PS1"'\n'                 # new line
+  # Taken from https://github.com/git-for-windows/build-extra/blob/main/git-extra/git-prompt.sh
+  PS1='\[\033]0;$TITLEPREFIX:${PWD//[^[:ascii:]]/?}\007\]' # set window title
+  # PS1="$PS1"'\n'               # new line (disabled)
   PS1="$PS1"'\[\033[32m\]'       # change to green
   PS1="$PS1"'\u@\h '             # user@host<space>
-  # PS1="$PS1"'\[\033[35m\]'       # change to purple
-  # PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
+  PS1="$PS1${MSYSTEM:+\[\033[35m\]$MSYSTEM }" # show MSYSTEM in purple (if set)
   PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
   PS1="$PS1"'\w'                 # current working directory
   if test -z "$WINELOADERNOEXEC"
@@ -68,7 +68,7 @@ else
   fi
   PS1="$PS1"'\[\033[0m\]'        # change color
   PS1="$PS1"'\n'                 # new line
-  PS1="$PS1"'λ '                 # prompt: always λ
+  PS1="$PS1"'λ '                 # prompt: Cmder uses λ
 fi
 
 MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
